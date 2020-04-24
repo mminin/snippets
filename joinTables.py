5d851b42b282fbd39255a1ad434370a146ee3360
Mikhail Minin <mminin2010@gmail.com>
2020-04-24 05:54:24 -0400

    Update joinTables.py
    bug fix in joinTables.py when no matches are found

--- joinTables.py
+++ joinTables.py
@@ -24,26 +24,32 @@ fields += functools.reduce(lambda a,b:a+b,[getNewKeys(*_) for _ in filesAndKeys[
 print(fields)
 
 allKeys=set.union(*[{_[k] for _ in f} for f,k in filesAndKeys])
-
+print(allKeys)
 
 print(filesAndKeys[0][1])
 
 
 def joinRow(k):
-    print(k)
+#    print(k)
     #tmp=[_ for _ in filesAndKeys[0][0] if _[filesAndKeys[0][1]]==k][0]
     tmp=dict()
-    print(tmp)
+#    print(tmp)
 #    for f, InKey in filesAndKeys[1:]:
     for f, InKey in filesAndKeys:
-        print(InKey)
-        print('DEBUG %s'%([_ for _ in f if _[InKey]==k][0]))
-        tmp.update([_ for _ in f if _[InKey]==k][0])
+        if k in allKeys:
+            klist=[_ for _ in f if _[InKey]==k]
+            if len(klist)>0:
+#                print('DEBUG %s'%(klist[0]))
+#                print('updating..')
+                tmp.update(klist[0])
+#                print('updated')
     return tmp
 
 with open("output.csv","w") as f:
     w=csv.DictWriter(f, fieldnames=fields)
     w.writeheader()
     for k in allKeys:
-        print(k)
-        w.writerow(joinRow(k))
+#        print(k)
+        row=joinRow(k)
+#        print(row)
+        w.writerow(row)
